a16125daa8a9680ba846af04ec52791bfdfb661e
teste <teste@INFO02>
2019-11-29 16:09:18 -0200

    Andre infod: model de curso

---
+++ Nsf.App.Model/CursoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nsf.App.Model
+{
+    class CursoModel
+    {
+        public int IdCurso { get; set; }
+        public string NmCurso { get; set; }
+        public string DsSigla { get; set; }
+
+        public int NrCapacidadeMaxima { get; set; }
+        public bool BtAtivo { get; set; }
+        public DateTime DtCriacao { get; set; }
+        public DateTime DtUltimaAlteracao { get; set; }
+        public string DsCategoria { get; set; }
+    }
+}
