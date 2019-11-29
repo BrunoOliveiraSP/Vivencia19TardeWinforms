df17285e5c75f7c8fbc56f49723942a27017c853
teste <teste@INFO03>
2019-11-29 13:17:38 -0300

    Thomaz Henrique: Tratamento de erro Inscrição

---
+++ Nsf.App.UI/API/InscricaoAPI.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nsf.App.UI.API
+{
+    class InscricaoAPI
+    {
+        public void Inserir(Models.InscricaoModel inscricao)
+        {
+            HttpClient client = new HttpClient();
+
+            string json = JsonConvert.SerializeObject(inscricao);
+            StringContent body = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var resp = client.PostAsync("http://localhost:5000/Inscricao", body)
+                             .Result
+                             .Content
+                             .ReadAsStringAsync()
+                             .Result;
+
+            this.VerificarErro(json);
+        }
+
+        private void VerificarErro(string respostaAPI)
+        {
+            if(respostaAPI.Contains("codigoErro"))
+            {
+                Models.ErrorModel erro = JsonConvert.DeserializeObject<Models.ErrorModel>(respostaAPI);
+                throw new ArgumentException(erro.Mensagem);
+            }
+        }
+    }
+}
---
+++ Nsf.App.UI/Models/ErrorModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nsf.App.UI.Models
+{
+    class ErrorModel
+    {
+        public int CodigoErro { get; set; }
+        public string Mensagem { get; set; }
+
+    }
+}
