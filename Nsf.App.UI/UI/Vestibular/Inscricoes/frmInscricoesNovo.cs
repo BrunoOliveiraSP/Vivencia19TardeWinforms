87e99a9df81a6f84d12419edcdb0d7a5b617a608
teste <teste@INFO21>
2019-11-29 16:47:03 -0300

    ...

--- Nsf.App.UI/UI/Vestibular/Inscricoes/frmInscricoesNovo.cs
+++ Nsf.App.UI/UI/Vestibular/Inscricoes/frmInscricoesNovo.cs
@@ -16,7 +16,7 @@ namespace Nsf.App.UI
 
         private void btnCoordenacao_Click(object sender, EventArgs e)
         {
-            Models.InscricaoModel inscricao = new Models.InscricaoModel();
+            Nsf.App.Model.InscricaoModel inscricao = new Nsf.App.Model.InscricaoModel();
             inscricao.CdInscricao = Convert.ToInt32(nudId.Value);
             inscricao.NmInscrito = txtNome.Text;
             inscricao.DtNascimento = Convert.ToDateTime(txtNascimentoData.Text);
