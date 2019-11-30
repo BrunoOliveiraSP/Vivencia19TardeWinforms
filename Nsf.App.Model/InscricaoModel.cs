504f1243a6e44cbbb9be2412a43153024173494f
Thomaz Henrique <treimberg7@gmail.com>
2019-11-30 00:53:21 -0300

    ...

--- Nsf.App.Model/InscricaoModel.cs
+++ Nsf.App.Model/InscricaoModel.cs
@@ -8,66 +8,72 @@ namespace Nsf.App.Model
 {
     public class InscricaoModel
     {
-        public int IdInscricao { get; set; }
-        public int IdAnoLetivo { get; set; }
-        public string NmInscrito { get; set; }
-        public DateTime DtNascimento { get; set; }
-        public string MyProDsRgperty { get; set; }
-        public DateTime DtEmissao { get; set; }
-        public string DsOrgao { get; set; }
-        public string DsSexo { get; set; }
-        public string DsCpf { get; set; }
-        public string DsNascimentoCidade { get; set; }
-        public string DsNascimentoEstado { get; set; }
-        public string DsNascimentoPais { get; set; }
-        public string DsResidenciaEndereco { get; set; }
-        public int NrResidenciaEndereco { get; set; }
-        public string DsResidenciaComplelemento { get; set; }
-        public string DsResidenciaBairro { get; set; }
-        public string DsResidenciaCidade { get; set; }
-        public string DsResidenciaEstado { get; set; }
-        public string DsResidenciaCep { get; set; }
-        public string DsEmail { get; set; }
-        public string DsTelefone { get; set; }
-        public string DsTelefone2 { get; set; }
-        public string NmContato { get; set; }
-        public string DsResponsavelNome { get; set; }
-        public string DsResponsavelEmail { get; set; }
-        public string DsResponsavelTelefone { get; set; }
-        public string DsResponsavelTelefone2 { get; set; }
-        public string DsResponsavelParentesco { get; set; }
-        public int IdCurso { get; set; }
-        public string DsCursoPeriodo { get; set; }
-        public int IdCurso2 { get; set; }
-        public string DsCurso2Periodo { get; set; }
-        public string DsEscolaridade { get; set; }
-        public string NmEscola { get; set; }
-        public string TpEscola { get; set; }
-        public decimal VlRenda { get; set; }
-        public string DsComoConheceu { get; set; }
-        public int QtMoramCasa { get; set; }
-        public int QtTrabalhamCasa { get; set; }
-        public string DsCorPele { get; set; }
-        public bool BtPendenteRg { get; set; }
-        public bool BtPendenteCpf { get; set; }
-        public bool BtPendenteEscolaridade { get; set; }
-        public bool BtPendentePagamento { get; set; }
-        public string DsObservacao { get; set; }
-        public DateTime DtInclusao { get; set; }
-        public DateTime DtUltimaAlteracao { get; set; }
-        public int IdFuncionarioAlteracao { get; set; }
-        public decimal VlNota { get; set; }
-        public int IdSalaVestibular { get; set; }
-        public string DsSituacao { get; set; }
-        public bool BtMatriculado { get; set; }
-        public bool BtPendenteComprovresid { get; set; }
-        public string DsResponsavelCpf { get; set; }
-        public string DsResponsavelRg { get; set; }
-        public int CdInscricao { get; set; }
-        public string ds_email_inscrito { get; set; }
-        public int DsRg { get; set; }
-
-
-        //Falta colocar as tabelas aqui também
+        public int idInscricao { get; set; }
+        public int idAnoLetivo { get; set; }
+        public string nmInscrito { get; set; }
+        public DateTime dtNascimento { get; set; }
+        public string dsRg { get; set; }
+        public DateTime dtEmissao { get; set; }
+        public string dsOrgao { get; set; }
+        public string dsSexo { get; set; }
+        public string dsCpf { get; set; }
+        public string dsNascimentoCidade { get; set; }
+        public string dsNascimentoEstado { get; set; }
+        public string dsNascimentoPais { get; set; }
+        public string dsResidenciaEndereco { get; set; }
+        public int nrResidenciaEndereco { get; set; }
+        public string dsResidenciaComplelemento { get; set; }
+        public string dsResidenciaBairro { get; set; }
+        public string dsResidenciaCidade { get; set; }
+        public string dsResidenciaEstado { get; set; }
+        public string dsResidenciaCep { get; set; }
+        public string dsEmail { get; set; }
+        public string dsTelefone { get; set; }
+        public string dsTelefone2 { get; set; }
+        public string nmContato { get; set; }
+        public string dsResponsavelNome { get; set; }
+        public string dsResponsavelEmail { get; set; }
+        public string dsResponsavelTelefone { get; set; }
+        public string dsResponsavelTelefone2 { get; set; }
+        public string dsResponsavelParentesco { get; set; }
+        public int idCurso { get; set; }
+        public string dsCursoPeriodo { get; set; }
+        public int idCurso2 { get; set; }
+        public string dsCurso2Periodo { get; set; }
+        public string dsEscolaridade { get; set; }
+        public string nmEscola { get; set; }
+        public string tpEscola { get; set; }
+        public decimal vlRenda { get; set; }
+        public string dsComoConheceu { get; set; }
+        public int qtMoramCasa { get; set; }
+        public int qtTrabalhamCasa { get; set; }
+        public string dsCorPele { get; set; }
+        public bool btPendenteRg { get; set; }
+        public bool btPendenteCpf { get; set; }
+        public bool btPendenteEscolaridade { get; set; }
+        public bool btPendentePagamento { get; set; }
+        public string dsObservacao { get; set; }
+        public DateTime dtInclusao { get; set; }
+        public DateTime dtUltimaAlteracao { get; set; }
+        public int idFuncionarioAlteracao { get; set; }
+        public decimal vlNota { get; set; }
+        public int idSalaVestibular { get; set; }
+        public string dsSituacao { get; set; }
+        public bool btMatriculado { get; set; }
+        public bool btPendenteComprovresid { get; set; }
+        public string dsResponsavelCpf { get; set; }
+        public string dsResponsavelRg { get; set; }
+        public int cdInscricao { get; set; }
+        public string dsEmailInscrito { get; set; }
+        //public object idAnoLetivoNavigation { get; set; }
+        //public object idCurso2Navigation { get; set; }
+        //public object idCursoNavigation { get; set; }
+        //public object idSalaVestibularNavigation { get; set; }
+        //public List<object> tbInscritoAprovado { get; set; }
+        //public List<object> tbTurmaInscricaoTransicao { get; set; }
     }
+
+
+    //Falta colocar as tabelas aqui também
 }
+
--- Nsf.App.UI/UI/Vestibular/Inscricoes/frmInscricoesNovo.cs
+++ Nsf.App.UI/UI/Vestibular/Inscricoes/frmInscricoesNovo.cs
@@ -21,53 +21,63 @@ namespace Nsf.App.UI
             try
             {
                 Model.InscricaoModel inscricao = new Model.InscricaoModel();
-                inscricao.CdInscricao = Convert.ToInt32(nudId.Value);
-                inscricao.NmInscrito = txtNome.Text;
-                inscricao.DtNascimento = Convert.ToDateTime(txtNascimentoData.Text);
-                inscricao.DsCpf = txtCep.Text;
-                inscricao.DsSexo = cboSexo.Text;
-                inscricao.DsCorPele = txtCorDaPele.Text;
-                inscricao.DsRg = Convert.ToInt32(txtRG.Text);
-                inscricao.DtEmissao = Convert.ToDateTime(dtpRgEmissao.Text);
-                inscricao.DsOrgao = txtRgOrgao.Text;
-                inscricao.DsEmail = txtEmailInscrito.Text;
-                inscricao.DsNascimentoCidade = txtNascimentoCidade.Text;
-                inscricao.DsNascimentoEstado = cboNascimentoUf.Text;
-                inscricao.DsNascimentoPais = txtNascimentoPais.Text;
-                inscricao.DsResidenciaCep = txtCep.Text;
-                inscricao.DsResidenciaEndereco = txtEndereco.Text;
-                inscricao.NrResidenciaEndereco = Convert.ToInt32(txtNumero.Text);
-                inscricao.DsResidenciaComplelemento = txtComplemento.Text;
-                inscricao.DsResidenciaBairro = txtBairro.Text;
-                inscricao.DsResidenciaEstado = txtCidade.Text;
-                inscricao.DsResidenciaCidade = txtCidade.Text;
-                // segunda guia
-                inscricao.DsResponsavelNome = txtContato.Text;
-                inscricao.DsTelefone2 = txtTelefone2.Text;
-                inscricao.DsTelefone = txtTelefone1.Text;
-                inscricao.DsResponsavelEmail = txtMaeEmail.Text;
-                inscricao.NmContato = txtResponsavel.Text;
-                inscricao.DsResponsavelTelefone = txtResponsavelTelefone1.Text;
-                inscricao.DsResponsavelTelefone2 = txtResponsavelTelefone2.Text;
-                inscricao.DsResponsavelEmail = txtResponsavelEmail.Text;
-                inscricao.DsResponsavelParentesco = cboGrauParentesco.Text;
-                inscricao.DsEscolaridade = cboEscolaridade.Text;
-                inscricao.TpEscola = cboTipoDeEscola.Text;
-                inscricao.NmEscola = txtNomeDaEscola.Text;
-                //Campo de curso precisa do modelo de curso pra continuar
-                //Campo de curso2 ''     ''   ''   ''  ''    ''    ''
-                inscricao.DsCursoPeriodo = cboTurno1.Text;
-                inscricao.DsCurso2Periodo = cboTurno2.Text;
-                //Terceira guia
-                inscricao.DsComoConheceu = cboComoConheceu.Text;
-                inscricao.VlRenda = Convert.ToDecimal(txtRenda.Text);
-                inscricao.QtMoramCasa = Convert.ToInt32(nudPessoasMoramCasa.Value);
-                inscricao.QtTrabalhamCasa = Convert.ToInt32(nudPessoasTrabalhamCasa.Value);
-                inscricao.BtPendenteCpf = chkPendenteCpf.Checked;
-                inscricao.BtPendenteRg = chkPendenteRg.Checked;
-                inscricao.BtPendenteEscolaridade = chkPendenteEscolaridade.Checked;
-                inscricao.BtPendenteComprovresid = chkPendenteComprovResidencia.Checked;
-                inscricao.DsObservacao = txtObservacoes.Text;
+                inscricao.btMatriculado = true;
+                inscricao.btPendenteComprovresid = false;
+                inscricao.btPendenteCpf = false;
+                inscricao.btPendenteEscolaridade = false;
+                inscricao.btPendentePagamento = false;
+                inscricao.btPendenteRg = true;
+                inscricao.cdInscricao = 7777;
+                inscricao.dsComoConheceu = "Conhecendo";
+                inscricao.dsCorPele = "Cor";
+                inscricao.dsCpf = "0007";
+                inscricao.dsCurso2Periodo = "Tarde";
+                inscricao.dsCursoPeriodo = "Tarde";
+                inscricao.dsEmail = "email@gmail.com";
+                inscricao.dsEmailInscrito = "email@gmail.com";
+                inscricao.dsEscolaridade = "Escola";
+                inscricao.dsNascimentoCidade = "Aqui mesmo";
+                inscricao.dsNascimentoEstado = "Tbm aqui";
+                inscricao.dsNascimentoPais = "Vera Cruz";
+                inscricao.dsObservacao = "Nenhuma";
+                inscricao.dsOrgao = "Supremo Tribunal";
+                inscricao.dsResidenciaBairro = "Arredores da minha casa";
+                inscricao.dsResidenciaCep = "0012";
+                inscricao.dsResidenciaCidade = "Aqui";
+                inscricao.dsResidenciaComplelemento = "Mesmo";
+                inscricao.dsResidenciaEndereco = "Já";
+                inscricao.dsResidenciaEstado = "Falei";
+                inscricao.dsResponsavelCpf = "Isso!";
+                inscricao.dsResponsavelEmail = "@";
+                inscricao.dsResponsavelNome = "Pai e Mãe";
+                inscricao.dsResponsavelParentesco = "O mesmo do anterior";
+                inscricao.dsResponsavelRg = "Não sei";
+                inscricao.dsResponsavelTelefone = "Pi R ao quadrado";
+                inscricao.dsResponsavelTelefone2 = "l Raiz de 2";
+                inscricao.dsRg = "Baskara";
+                inscricao.dsSexo = "Pitágoras";
+                inscricao.dsSituacao = "Legal";
+                inscricao.dsTelefone = "42";
+                inscricao.dsTelefone2 = "IA";
+                inscricao.dtEmissao = DateTime.Now;
+                inscricao.dtInclusao = DateTime.Now;
+                inscricao.dtNascimento = DateTime.Now.Date;
+                inscricao.dtUltimaAlteracao = DateTime.Now;
+                inscricao.idAnoLetivo = 1;
+                inscricao.idCurso = 1;
+                inscricao.idCurso2 = 1;
+                inscricao.idFuncionarioAlteracao = 1;
+                inscricao.idInscricao = 1;
+                inscricao.idSalaVestibular = 1;
+                inscricao.nmContato = "Contato";
+                inscricao.nmEscola = "Escola";
+                inscricao.nmInscrito = "Eu :)";
+                inscricao.nrResidenciaEndereco = 1;
+                inscricao.qtMoramCasa = 2;
+                inscricao.qtTrabalhamCasa = 2;
+                inscricao.tpEscola = "-_-";
+                inscricao.vlNota = 10;
+                inscricao.vlRenda = 0.00m;
 
                 Api.Inserir(inscricao);
 
@@ -85,5 +95,51 @@ namespace Nsf.App.UI
             //Carregar os dois combos da tela
         }
         
-        }
     }
+
+}
+//inscricao.DtNascimento = Convert.ToDateTime(txtNascimentoData.Text);
+//inscricao.DsCpf = txtCep.Text;
+//inscricao.DsSexo = cboSexo.Text;
+//inscricao.DsCorPele = txtCorDaPele.Text;
+//inscricao.DsRg = Convert.ToInt32(txtRG.Text);
+//inscricao.DtEmissao = Convert.ToDateTime(dtpRgEmissao.Text);
+//inscricao.DsOrgao = txtRgOrgao.Text;
+//inscricao.DsEmail = txtEmailInscrito.Text;
+//inscricao.DsNascimentoCidade = txtNascimentoCidade.Text;
+//inscricao.DsNascimentoEstado = cboNascimentoUf.Text;
+//inscricao.DsNascimentoPais = txtNascimentoPais.Text;
+//inscricao.DsResidenciaCep = txtCep.Text;
+//inscricao.DsResidenciaEndereco = txtEndereco.Text;
+//inscricao.NrResidenciaEndereco = Convert.ToInt32(txtNumero.Text);
+//inscricao.DsResidenciaComplelemento = txtComplemento.Text;
+//inscricao.DsResidenciaBairro = txtBairro.Text;
+//inscricao.DsResidenciaEstado = txtCidade.Text;
+//inscricao.DsResidenciaCidade = txtCidade.Text;
+// segunda guia
+//inscricao.DsResponsavelNome = txtContato.Text;
+//inscricao.DsTelefone2 = txtTelefone2.Text;
+//inscricao.DsTelefone = txtTelefone1.Text;
+//inscricao.DsResponsavelEmail = txtMaeEmail.Text;
+//inscricao.NmContato = txtResponsavel.Text;
+//inscricao.DsResponsavelTelefone = txtResponsavelTelefone1.Text;
+//inscricao.DsResponsavelTelefone2 = txtResponsavelTelefone2.Text;
+//inscricao.DsResponsavelEmail = txtResponsavelEmail.Text;
+//inscricao.DsResponsavelParentesco = cboGrauParentesco.Text;
+//inscricao.DsEscolaridade = cboEscolaridade.Text;
+//inscricao.TpEscola = cboTipoDeEscola.Text;
+//inscricao.NmEscola = txtNomeDaEscola.Text;
+//Campo de curso precisa do modelo de curso pra continuar
+//Campo de curso2 ''     ''   ''   ''  ''    ''    ''
+//inscricao.DsCursoPeriodo = cboTurno1.Text;
+//inscricao.DsCurso2Periodo = cboTurno2.Text;
+//Terceira guia
+//inscricao.DsComoConheceu = cboComoConheceu.Text;
+//inscricao.VlRenda = Convert.ToDecimal(txtRenda.Text);
+//inscricao.QtMoramCasa = Convert.ToInt32(nudPessoasMoramCasa.Value);
+//inscricao.QtTrabalhamCasa = Convert.ToInt32(nudPessoasTrabalhamCasa.Value);
+//inscricao.BtPendenteCpf = chkPendenteCpf.Checked;
+//inscricao.BtPendenteRg = chkPendenteRg.Checked;
+//inscricao.BtPendenteEscolaridade = chkPendenteEscolaridade.Checked;
+//inscricao.BtPendenteComprovresid = chkPendenteComprovResidencia.Checked;
+//inscricao.DsObservacao = txtObservacoes.Text;
