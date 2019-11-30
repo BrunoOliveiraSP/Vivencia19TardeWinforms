17c529d624140b77a5d7e25aa617888001cb3cf6
João Pedro Santos Costa <joaopedro.cs12@hotmail.com>
2019-11-30 01:08:04 -0300

    João Pedro: Inserir Disciplina não salva no Banco, " mas sem erro de cod"

---
+++ Nsf.App.API.Client/DisciplinaAPI.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nsf.App.API.Client
+{
+    public  class DisciplinaAPI
+    {
+      
+
+        public void Inserir(Nsf.App.Model.DisciplinaModel model)
+        {
+            HttpClient client = new HttpClient();
+
+            string json = JsonConvert.SerializeObject(model);
+            StringContent body = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var resposta = client.PostAsync("http://localhost:5000/Disciplina", body).Result;
+        }
+    }
+}
--- Nsf.App.UI/UI/Coordenacao/Disciplinas/frmDisciplinasCadastrar.cs
+++ Nsf.App.UI/UI/Coordenacao/Disciplinas/frmDisciplinasCadastrar.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace Nsf.App.UI
 {
@@ -10,6 +11,24 @@ namespace Nsf.App.UI
             InitializeComponent();
         }
 
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Nsf.App.Model.DisciplinaModel model = new Model.DisciplinaModel();
+
+
+            model.NmDisciplina = txtDisciplina.Text;
+            model.DsSigla = txtSigla.Text;
+            model.DtInclusao = dtpinclusao.Value;
+            model.BtAtivo = chkAtivo.Checked;
+
+
+            Nsf.App.API.Client.DisciplinaAPI api = new App.API.Client.DisciplinaAPI();
+
+            api.Inserir(model);
+
+            MessageBox.Show("Disciplina Inserida com sucesso.");
+
+        }
     }
 
 }
