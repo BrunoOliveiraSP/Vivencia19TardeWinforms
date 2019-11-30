2821b6f15098032a302afc14c803d7b3d40949f2
Thomaz Henrique <treimberg7@gmail.com>
2019-11-30 15:48:42 -0300

    Thomaz Henrique: Funcionalidade de Remoção funcionando (Inscrição)

--- Nsf.App.UI/UI/Vestibular/Inscricoes/frmInscricoesNovo.cs
+++ Nsf.App.UI/UI/Vestibular/Inscricoes/frmInscricoesNovo.cs
@@ -67,7 +67,7 @@ namespace Nsf.App.UI
                 inscricao.idCurso = 2;
                 inscricao.idCurso2 = 2;
                 inscricao.idFuncionarioAlteracao = 1;
-                //inscricao.idInscricao = 1;
+                inscricao.idInscricao = 0;
                 inscricao.idSalaVestibular = 1;
                 inscricao.nmContato = txtContato.Text;
                 inscricao.nmEscola = txtNomeDaEscola.Text;
@@ -98,48 +98,3 @@ namespace Nsf.App.UI
     }
 
 }
-//inscricao.DtNascimento = Convert.ToDateTime(txtNascimentoData.Text);
-//inscricao.DsCpf = txtCep.Text;
-//inscricao.DsSexo = cboSexo.Text;
-//inscricao.DsCorPele = txtCorDaPele.Text;
-//inscricao.DsRg = Convert.ToInt32(txtRG.Text);
-//inscricao.DtEmissao = Convert.ToDateTime(dtpRgEmissao.Text);
-//inscricao.DsOrgao = txtRgOrgao.Text;
-//inscricao.DsEmail = txtEmailInscrito.Text;
-//inscricao.DsNascimentoCidade = txtNascimentoCidade.Text;
-//inscricao.DsNascimentoEstado = cboNascimentoUf.Text;
-//inscricao.DsNascimentoPais = txtNascimentoPais.Text;
-//inscricao.DsResidenciaCep = txtCep.Text;
-//inscricao.DsResidenciaEndereco = txtEndereco.Text;
-//inscricao.NrResidenciaEndereco = Convert.ToInt32(txtNumero.Text);
-//inscricao.DsResidenciaComplelemento = txtComplemento.Text;
-//inscricao.DsResidenciaBairro = txtBairro.Text;
-//inscricao.DsResidenciaEstado = txtCidade.Text;
-//inscricao.DsResidenciaCidade = txtCidade.Text;
-// segunda guia
-//inscricao.DsResponsavelNome = txtContato.Text;
-//inscricao.DsTelefone2 = txtTelefone2.Text;
-//inscricao.DsTelefone = txtTelefone1.Text;
-//inscricao.DsResponsavelEmail = txtMaeEmail.Text;
-//inscricao.NmContato = txtResponsavel.Text;
-//inscricao.DsResponsavelTelefone = txtResponsavelTelefone1.Text;
-//inscricao.DsResponsavelTelefone2 = txtResponsavelTelefone2.Text;
-//inscricao.DsResponsavelEmail = txtResponsavelEmail.Text;
-//inscricao.DsResponsavelParentesco = cboGrauParentesco.Text;
-//inscricao.DsEscolaridade = cboEscolaridade.Text;
-//inscricao.TpEscola = cboTipoDeEscola.Text;
-//inscricao.NmEscola = txtNomeDaEscola.Text;
-//Campo de curso precisa do modelo de curso pra continuar
-//Campo de curso2 ''     ''   ''   ''  ''    ''    ''
-//inscricao.DsCursoPeriodo = cboTurno1.Text;
-//inscricao.DsCurso2Periodo = cboTurno2.Text;
-//Terceira guia
-//inscricao.DsComoConheceu = cboComoConheceu.Text;
-//inscricao.VlRenda = Convert.ToDecimal(txtRenda.Text);
-//inscricao.QtMoramCasa = Convert.ToInt32(nudPessoasMoramCasa.Value);
-//inscricao.QtTrabalhamCasa = Convert.ToInt32(nudPessoasTrabalhamCasa.Value);
-//inscricao.BtPendenteCpf = chkPendenteCpf.Checked;
-//inscricao.BtPendenteRg = chkPendenteRg.Checked;
-//inscricao.BtPendenteEscolaridade = chkPendenteEscolaridade.Checked;
-//inscricao.BtPendenteComprovresid = chkPendenteComprovResidencia.Checked;
-//inscricao.DsObservacao = txtObservacoes.Text;
