0354b8c84f819e55448a485bcec6d833e55c0f32
Bruno de Oliveira <brunooliveira.me@outlook.com>
2019-11-30 09:50:11 -0300

    .

--- Nsf.App.API.Client/DisciplinaAPI.cs
+++ Nsf.App.API.Client/DisciplinaAPI.cs
@@ -10,16 +10,45 @@ namespace Nsf.App.API.Client
 {
     public  class DisciplinaAPI
     {
-      
 
-        public void Inserir(Nsf.App.Model.DisciplinaModel model)
+        public void Inserir(Nsf.App.Model.DisciplinaModel disci)
         {
             HttpClient client = new HttpClient();
 
-            string json = JsonConvert.SerializeObject(model);
+            string json = JsonConvert.SerializeObject(disci);
             StringContent body = new StringContent(json, Encoding.UTF8, "application/json");
 
-            var resposta = client.PostAsync("http://localhost:5000/Disciplina", body).Result;
+            var resp = client.PostAsync("http://localhost:5000/Disciplina", body)
+                             .Result
+                             .Content
+                             .ReadAsStringAsync()
+                             .Result;
+
+            VerificarErro(resp);
+        }
+        public List<Nsf.App.Model.DisciplinaModel> Listar()
+        {
+            HttpClient client = new HttpClient();
+
+            string json = client.GetAsync("http://localhost:5000/Disciplina")
+                                .Result
+                                .Content
+                                .ReadAsStringAsync()
+                                .Result;
+
+            List<Nsf.App.Model.DisciplinaModel> lista =
+                JsonConvert.DeserializeObject<List<Nsf.App.Model.DisciplinaModel>>(json);
+
+            return lista;
+        }
+
+        private void VerificarErro(string respostaAPI)
+        {
+            if (respostaAPI.Contains("codigoErro"))
+            {
+                Model.ErroModel erro = JsonConvert.DeserializeObject<Model.ErroModel>(respostaAPI);
+                throw new ArgumentException(erro.Mensagem);
+            }
         }
     }
 }
