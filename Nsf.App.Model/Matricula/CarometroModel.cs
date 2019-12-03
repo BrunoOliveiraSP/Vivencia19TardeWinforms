af57f06dbbbf211b73250d26ce88562c93a9869e
freivivencia2019 <freivivencia2019@gmail.com>
2019-12-03 13:23:18 -0300

    Leandro Infoc 24 Models matricula

---
+++ Nsf.App.Model/Matricula/CarometroModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Nsf.App.Model.Matricula
+{
+    public class CaromroetModel
+    {
+        public int IdAlunoCarometro { get; set; }
+        public int IdAluno { get; set; }
+        public string DsImagem { get; set; }
+    }
+}
