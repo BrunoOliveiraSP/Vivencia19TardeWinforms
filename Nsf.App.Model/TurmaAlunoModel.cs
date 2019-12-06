c6a7ce8214774e9949e33df940632c8799b48edd
freivivencia2019 <freivivencia2019@gmail.com>
2019-12-05 23:11:50 -0300

    Leandro Infoc24 Model TurmaAluno

---
+++ Nsf.App.Model/TurmaAlunoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nsf.App.Model
+{
+    public class SalaVestibularModel
+    {
+        public int IdTurmaAluno { get; set; }
+        public int IdTurma { get; set; }
+        public string IdAluno { get; set; }
+        public int NmTurma { get; set; }
+        public int NrChamada { get; set; }
+        public int CdRa { get; set; }
+        public int TpStatus { get; set; }
+        public int BtAtivo { get; set; }
+        public int BtPermissaoAtraso { get; set; }
+        public int DsObservacao { get; set; }
+
+    }
+}
