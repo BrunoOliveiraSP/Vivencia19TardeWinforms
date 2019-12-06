755a5b076bffcf9121735d175d66d8ae614ea7c6
Windows <Windows@WINDOWS-PC>
2019-12-05 22:20:42 -0300

    salavestibular

--- Nsf.App.UI/UI/Coordenacao/Salas/Aula/frmSalaCadastrar.cs
+++ Nsf.App.UI/UI/Coordenacao/Salas/Aula/frmSalaCadastrar.cs
@@ -3,12 +3,12 @@ using System.Windows.Forms;
 
 namespace Nsf.App.UI
 {
-	public partial class frmSalaCadastrar : NsfUserScreen
-	{
-		public frmSalaCadastrar()
-		{
-			InitializeComponent();
-		}
+    public partial class frmSalaCadastrar : NsfUserScreen
+    {
+        public frmSalaCadastrar()
+        {
+            InitializeComponent();
+        }
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
@@ -23,6 +23,52 @@ namespace Nsf.App.UI
             api.inserir(sala);
 
             MessageBox.Show("Cadastrado com sucesso");
+        }
+
+        public void Alterar()
+        {
+
+            try
+            {
+                Nsf.App.Model.SalaModel sala = new Model.SalaModel();
+
+                sala.NmSala = txtSala.Text;
+                sala.NmLocal = cboInstituicao.Text;
+                sala.NrCapacidadeMaxima = Convert.ToInt32(nudCapacidade.Value);
+                sala.BtAtivo = Convert.ToBoolean(chkAtivo.Checked);
+
+                frmSalaCadastrar tela = new frmSalaCadastrar();
+
+                frmInicial.Current.OpenScreen(tela);
+                tela.CarregarCampos(sala);
+
+
+
+                MessageBox.Show("Sala Alterada com sucesso.");
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Erro Grave Renicie Esta Maquina");
+            }
+        }
+        public void CarregarCampos(Model.SalaModel model)
+        {
+            try
+            {
+                Model.SalaModel sala = new Model.SalaModel();
+                panelId.Visible = true;
+
+
+                txtSala.Text = sala.NmSala;
+                cboInstituicao.Text = sala.NmLocal;
+                nudCapacidade.Value = sala.NrCapacidadeMaxima;
+                chkAtivo.Checked = sala.BtAtivo;
+
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Erro Grave Rencie A Maquina");
+            }
         }
     }
 }
