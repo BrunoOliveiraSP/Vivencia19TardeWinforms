0226eed46be09cd898023bddb2ff6ce50a2e9be5
mayara silva <mayara698015@hotmail.com>
2019-12-09 15:26:18 -0300

    Gabriel Info c 11 Matricula

--- Nsf.App.UI/UI/Vestibular/Matricula/frmMatriculaConsultar.cs
+++ Nsf.App.UI/UI/Vestibular/Matricula/frmMatriculaConsultar.cs
@@ -10,17 +10,38 @@ namespace Nsf.App.UI
 		{
 			InitializeComponent();
 		}
+        Nsf.App.API.Client.MatriculaAPI MatriculaApi = new App.API.Client.MatriculaAPI();
 
         private void dgvCandidatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             if(e.ColumnIndex == 9)
             {
-                Nsf.App.API.Client.MatriculaAPI api = new App.API.Client.MatriculaAPI();
+                
 
-                Model.MatriculaRequest matricula = 
+                Model.MatriculaRequest matricula = dgvCandidatos.CurrentRow.DataBoundItem as Model.MatriculaRequest;
 
-                api.Deletar()
+                DialogResult result = MessageBox.Show("Dejesa Remover?", "NSF", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (result == DialogResult.Yes)
+                {
+                    MatriculaApi.Deletar(matricula.Aluno.IdAluno);
+                    MessageBox.Show("Registro removido", "NSF", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    CarregarGrid();
+                }
             }
+            else if(e.ColumnIndex == 8)
+            {
+                Model.MatriculaRequest matricula = dgvCandidatos.CurrentRow.DataBoundItem as Model.MatriculaRequest;
+
+                Model.MatriculaRequest novo = new Model.MatriculaRequest();
+
+                novo.Aluno.NmAluno = matricula.Aluno.NmAluno;
+                novo.Aluno.DsRg = matricula.Aluno.DsRg;
+            }
+        }
+
+        private List<Model.MatriculaRequest> CarregarGrid()
+        {
+            MatriculaApi.
         }
     }
 }
