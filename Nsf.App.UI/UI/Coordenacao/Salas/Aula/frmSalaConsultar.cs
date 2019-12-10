e7dd99d79809ecf3ea10142fb14d9b53c62029de
teste <teste@INFO03>
2019-12-10 13:07:21 -0300

    sala

--- Nsf.App.UI/UI/Coordenacao/Salas/Aula/frmSalaConsultar.cs
+++ Nsf.App.UI/UI/Coordenacao/Salas/Aula/frmSalaConsultar.cs
@@ -4,34 +4,27 @@ using System.Windows.Forms;
 
 namespace Nsf.App.UI
 {
-	public partial class frmSalaConsultar : NsfUserScreen
-	{
-		public frmSalaConsultar()
-		{
-			InitializeComponent();
-		}
+    public partial class frmSalaConsultar : NsfUserScreen, INsfUserScreenConsult
+    {
+        public frmSalaConsultar()
+        {
+            InitializeComponent();
+        }
+
+
+        public void OnBackedScreen()
+        {
+            this.Consultar();
+        }
+
 
         public void Consultar()
         {
-            try
-            {
-                Nsf.App.API.Client.SalaAPI api = new App.API.Client.SalaAPI();
-                List<Model.SalaModel> teste = api.ListarTudo();
+            Nsf.App.API.Client.SalaAPI api = new App.API.Client.SalaAPI();
+            List<Model.SalaModel> teste = api.ListarTudo();
 
-                DataGridSalas.AutoGenerateColumns = false;
-                DataGridSalas.DataSource = teste;
-            }
-            catch (ArgumentException ex)
-            {
-                MessageBox.Show(ex.Message, "NSF", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Ocorreu um erro. Entre em contato com o administrador.", "NSF",
-                           MessageBoxButtons.OK,
-                           MessageBoxIcon.Error);
-            }
-
+            DataGridSalas.AutoGenerateColumns = false;
+            DataGridSalas.DataSource = teste;
         }
 
         private void frmSalaConsultar_Load(object sender, EventArgs e)
@@ -42,86 +35,65 @@ namespace Nsf.App.UI
 
         private void DataGridSalas_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            try
+
+            if (e.ColumnIndex == 5)
             {
-                if (e.ColumnIndex == 5)
+                Model.SalaModel teste = DataGridSalas.CurrentRow.DataBoundItem as Model.SalaModel;
+
+                DialogResult resp = MessageBox.Show("Deseja realmente excluir este dado?", "NSF", MessageBoxButtons.YesNo);
+                if (resp == DialogResult.Yes)
                 {
-                    Model.SalaModel teste = DataGridSalas.CurrentRow.DataBoundItem as Model.SalaModel;
+                    Nsf.App.API.Client.SalaAPI api = new App.API.Client.SalaAPI();
+                    api.Remover(teste.IdSala);
 
-                    DialogResult resp = MessageBox.Show("Deseja realmente excluir este dado?", "NSF", MessageBoxButtons.YesNo);
-                    if (resp == DialogResult.Yes)
-                    {
-                        Nsf.App.API.Client.SalaAPI api = new App.API.Client.SalaAPI();
-                        api.Remover(teste.IdSala);
+                    MessageBox.Show("Removido com sucesso!", "NSF");
 
-                        MessageBox.Show("Removido com sucesso!", "NSF");
-
-                        this.Consultar();
-                    }
-                }
-
-                if (e.ColumnIndex == 4)
-                {
-                    Model.SalaModel sala = DataGridSalas.CurrentRow.DataBoundItem as Model.SalaModel;
-
-                    Nsf.App.Model.SalaModel model = new Model.SalaModel();
-
-                    model.NrCapacidadeMaxima = sala.NrCapacidadeMaxima;
-                    model.DtAlteracao = sala.DtAlteracao;
-                    model.DtInclusao = sala.DtInclusao;
-                    model.NmLocal = sala.NmLocal;
-                    model.BtAtivo = sala.BtAtivo;
-                    model.IdSala = sala.IdSala;
-                    model.NmSala = sala.NmSala;
-
-                    Consultar();
-
-                    frmSalaCadastrar tela = new frmSalaCadastrar();
-
-                    frmInicial.Current.OpenScreen(tela);
-                    tela.CarregarCampos(model);
-
+                    this.Consultar();
                 }
             }
-            catch (ArgumentException ex)
+            if (e.ColumnIndex == 4)
             {
-                MessageBox.Show(ex.Message, "NSF", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Ocorreu um erro. Entre em contato com o administrador.", "NSF",
-                           MessageBoxButtons.OK,
-                           MessageBoxIcon.Error);
+                Model.SalaModel sala = DataGridSalas.CurrentRow.DataBoundItem as Model.SalaModel;
+
+                Nsf.App.Model.SalaModel model = new Model.SalaModel();
+
+                model.IdSala = sala.IdSala;
+                model.NmSala = sala.NmSala;
+                model.NmLocal = sala.NmLocal;
+                model.NrCapacidadeMaxima = sala.NrCapacidadeMaxima;
+                model.DtInclusao = sala.DtInclusao;
+                model.BtAtivo = sala.BtAtivo;
+                model.DtAlteracao = sala.DtAlteracao;
+                Consultar();
+                frmSalaCadastrar tela = new frmSalaCadastrar();
+                this.Consultar();
+
+                frmInicial.Current.OpenScreen(tela);
+                tela.CarregarCampos(model);
+
             }
         }
 
         private void txtInstituicao_TextChanged(object sender, EventArgs e)
         {
-            try
-            {
-                string insti = txtInstituicao.Text;
+            string insti = txtInstituicao.Text;
 
-                Nsf.App.API.Client.SalaAPI api = new App.API.Client.SalaAPI();
-                List<Model.SalaModel> teste = api.ConsultaPorInstituicao(insti);
+            Nsf.App.API.Client.SalaAPI api = new App.API.Client.SalaAPI();
+            List<Model.SalaModel> teste = api.ConsultaPorInstituicao(insti);
 
-                DataGridSalas.AutoGenerateColumns = false;
-                DataGridSalas.DataSource = teste;
+            DataGridSalas.AutoGenerateColumns = false;
+            DataGridSalas.DataSource = teste;
 
-                if (txtInstituicao.Text == String.Empty)
-                {
-                    this.Consultar();
-                }
-            }
-            catch (ArgumentException ex)
+            if (txtInstituicao.Text == String.Empty)
             {
-                MessageBox.Show(ex.Message, "NSF", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Ocorreu um erro. Entre em contato com o administrador.", "NSF",
-                           MessageBoxButtons.OK,
-                           MessageBoxIcon.Error);
+                this.Consultar();
             }
         }
+
+        private void DataGridSalas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
     }
 }
