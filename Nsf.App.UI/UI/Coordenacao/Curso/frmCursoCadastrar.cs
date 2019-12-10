5274cad729002e5b11b2058810c28bb25bed87cb
Seu <Seu@SEU-PC>
2019-12-10 14:17:59 -0300

    .

--- Nsf.App.UI/UI/Coordenacao/Curso/frmCursoCadastrar.cs
+++ Nsf.App.UI/UI/Coordenacao/Curso/frmCursoCadastrar.cs
@@ -25,9 +25,16 @@ namespace Nsf.App.UI
         private void btnSalvar_Click(object sender, EventArgs e)
         {
             int idCursoCondicao = Convert.ToInt32(lblID.Text);
+            string lbxDisciplinaCurso = Convert.ToString(lbxDisciplinasDoCurso.DataSource);
           
+            if (lbxDisciplinaCurso == null || lbxDisciplinaCurso == "")
+            {
+                tabControl1.SelectedTab = tabPage2;
+                MessageBox.Show("A atribuição de disciplinas ao curso é obrigatória", "Exigencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
 
-             if (idCursoCondicao == 0 ) //&& idCursoCondicao == null
+
+            else if (idCursoCondicao == 0 ) 
             {
                 try
                 {
@@ -211,13 +218,6 @@ namespace Nsf.App.UI
             Model.CursoDisciplinaModel mod = new Model.CursoDisciplinaModel();
 
             Nsf.App.API.Client.DisciplinaAPI api = new App.API.Client.DisciplinaAPI();
-
-            if (lbxDisciplinasDoCurso.DataSource == null)
-            {
-                tabControl1.SelectedTab = tabPage2;
-                throw new ArgumentException("A atribuição de disciplinas ao curso é obrigatória");
-                
-            }
 
             foreach (var item in atribuidas)
             {
--- Nsf.App.UI/UI/Vestibular/AnoLetivo/frmAnoLetivoCadastrar.cs
+++ Nsf.App.UI/UI/Vestibular/AnoLetivo/frmAnoLetivoCadastrar.cs
@@ -145,7 +145,7 @@ namespace Nsf.App.UI
             {
                 MessageBox.Show(ex.Message);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 MessageBox.Show("Ocorreu um erro", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
